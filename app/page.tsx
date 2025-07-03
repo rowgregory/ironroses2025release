62a39643dffc2bda14276b72f8e5c45a2012ee68
Gregory Row <rowgregory@gmail.com>
2025-07-02 20:59:34 -0400

    chore(home):Remove padding bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-dvh p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-pink-500 font-bold text-3xl">The Iron Roses</h1>
     </div>
   );
